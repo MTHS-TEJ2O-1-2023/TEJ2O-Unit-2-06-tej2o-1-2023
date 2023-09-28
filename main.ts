2cd095c012b331047ee30382b7acf5ad51f43c09
TEJ2O-1-2023 <144263858+tej2o-1-2023@users.noreply.github.com>
2023-09-28 13:31:05 -0400

    initial setup

--- main.ts
+++ main.ts
@@ -2,7 +2,14 @@
  *
  * Created by: Mr. Coxall
  * Created on: Sep 2020
- * This program ...
+ * This program turns an LED on a breadboard on and off.
 */
 
-basic.showString('Hello, World!')
+// setup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// turn on LED
+
+
+// turn off LED
